a441635a332a10df1af1617a89d312301d535dfb
Diogo Graciano <diogo.dg691@gmail.com>
2025-03-24 20:25:08 -0300

    adicionado-novas-teclas-de-acao-e-novo-botao

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,12 @@
 const nameInput = document.getElementById('name');
 const messageP = document.getElementById('message');
+const btnShowMessage = document.getElementById('btn-show-message');
+const btnClearMessage = document.getElementById('btn-clear-message');
+
+const clear = () => {
+    nameInput.value = '';
+    messageP.innerText = "";
+}
 
 nameInput.onkeyup = (event) => {
     if (nameInput.value === '') {
@@ -7,6 +14,20 @@ nameInput.onkeyup = (event) => {
     }
 }
 
-const helloWorld = () => {
+nameInput.onkeydown = (event) => {
+    if(event.keyCode == 13){
+        messageP.innerText = `Hello World, ${nameInput.value}!`;
+    }
+
+    if(event.keyCode == 27){
+        clear();
+    }
+}
+
+btnClearMessage.onclick = (event) => {
+    clear();
+}
+
+btnShowMessage.onclick = (event) => {
     messageP.innerText = `Hello World, ${nameInput.value}!`;
 }
